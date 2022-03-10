edad4f535638833b1cd860d6b16b999d60c6ede2
kiagekwamboka <kiagew@gmail.com>
2022-03-10 15:02:45 -0800

    first s script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 
+/**
+ * main function -prints message
+ */
 int main(void)
 {
 	char str[100] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
+
+	puts (str);
 	return(0);
 }	
