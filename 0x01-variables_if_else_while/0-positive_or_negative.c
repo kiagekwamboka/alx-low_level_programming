543086c57f3f3510c18888216ac9f24de4fcfb05
kiagekwamboka <kiagew@gmail.com>
2022-03-11 07:02:16 -0800

    first conditional

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/* more headers goes there */
+/**
+ * main - start of the program
+ * Return: 0 return success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("is positive");
+	else if (n == 0)
+		printf("is zero");
+	else
+		printf("is negative");
+	return (0);
+}
+
