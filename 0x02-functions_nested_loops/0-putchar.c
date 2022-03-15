72e9f0c6452a1d31a5e71ea01c3f5e7f640a0681
kiagekwamboka <kiagew@gmail.com>
2022-03-15 15:45:51 -0700

    nest loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* main - entry
+* Return: always a success
+*/
+int main(void)
+{
+	char i[20] = "_putchar";
+
+	printf("%s ", i);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+* main - Entry point
+* print_alphabet - prints the alphabet
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	char s;
+
+	for (s = 'a'; s <= 'z'; s++)
+		putchar(s);
+	putchar('\n');
+	getchar();
+}
