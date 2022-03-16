7bc6b77924a8a901f5856dc7632f53222943b4f6
kiagekwamboka <kiagew@gmail.com>
2022-03-16 15:07:37 -0700

    task 10 an 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - find sum
+ *@a: arguments
+* @b: argument to be computed
+ * Return: always success
+ */
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+
+	return (res);
+}
