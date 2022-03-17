b3871f89749ea11f77444ac6c5aadaa788ed3eaf
kiagekwamboka <kiagew@gmail.com>
2022-03-17 02:50:32 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -start execution
+ * Return: always asuccess
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+Debugging tasks
+1. create two main files
+2. Commenting out the main file 
+3. Print the correct output
+4. Change so that the output works correctly.
