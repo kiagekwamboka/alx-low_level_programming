31da354ca3a624358de94dce0a7f02ffda23ccd5
kiagekwamboka <kiagew@gmail.com>
2022-03-17 05:02:44 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul - multiply two integers
+ *@a: test value
+ *@b: test value
+ *Return: always success
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
