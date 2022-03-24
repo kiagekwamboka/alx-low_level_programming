91e279af63f058e171ccece2311e3b43a5a74e76
kiagekwamboka <kiage@gmail.com>
2022-03-24 14:23:08 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into
+ * @s: string to be encoded
+ * Return: returns encoded string
+ */
+char *leet(char *s)
+{
+	int i = 0, j;
+
+	char let[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
+
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (let[j] == s[i])
+			{
+				s[i] = num[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
