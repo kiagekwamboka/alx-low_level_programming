97d76139d2fa58b6ec2bfbf6ee2d5cf2cd7878e1
kiagekwamboka <kiage@gmail.com>
2022-03-24 13:59:00 -0700

    task to

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,6 +7,24 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	*dest = *dest + *src;
-	return (0);
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+		j++;
+	}
+
+	i = 0;
+	while (*(src + i) != '\0')
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
 }
